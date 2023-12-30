4732560b2b69a20029b99695839fe077faa00547
JosephLHub <125474970+JosephLHub@users.noreply.github.com>
2023-12-30 21:36:44 +0000

    added column pruning

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Human Friendly Sudoku Solver
 
 A sudoku solver based on methodologies more familiar to humans rather than brute force solutions.
+
+Currently implemented strategies:
+- Removing solved numbers in each row from possibilities of remaining unsolved numbers in said row
+- Removing solved numbers in each column from possibilities of remaining unsolved numbers in said column
+- Removing solved numbers in each 3x3 square from possibilities of remaining unsolved numbers in said 3x3 square
--- solver.py
+++ solver.py
@@ -30,21 +30,23 @@ for tile in range(len(example_board)): #Convert all 0s to lists of possible numb
     if example_board[tile] == 0:
         example_board[tile] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-solved = False
-
-def check_solved(board): #Check if every tile only has 1 possible number
-    return all([isinstance(tile, int) for tile in board])
-
 def axes_prune(board): #add column checks
     for row in range(9):
         nums = [x for x in board[row * 9 : (row * 9) + 9] if isinstance(x, int)]
         for tile in range(9):
-            for num in nums:
-                if not isinstance(board[(row * 9) + tile], int):
-                    board[(row * 9) + tile].remove(num)
+            if not isinstance(board[(row * 9) + tile], int):
+                board[(row * 9) + tile] = [num for num in board[(row * 9) + tile] if num not in nums] #Remove all other numbers in row from possibilities
+    for col in range(9):
+        nums = []
+        for x in range(9):
+            if isinstance(board[(x * 9) + col], int):
+                nums.append(board[(x * 9) + col])
+        for tile in range(9):
+            if not isinstance(board[(tile * 9) + col], int):
+                board[(tile * 9) + col] = [num for num in board[(tile * 9) + col] if num not in nums]
     return board
 
-def subsquares_prune(board):
+def subsquares_prune(board): #Remove solved numbers from possibilities in each 3x3 square
     subsquares = get_subsquares(board)
     for square in range(9):
         nums = [x for x in subsquares[square] if isinstance(x, int)] #Get all solved numbers in subsquare
@@ -58,12 +60,30 @@ def format_board(board): #Convert 1-long tile possibilities to solved numbers
     for tile in range(len(board)):
         if not isinstance(board[tile], int) and len(board[tile]) == 1:
             board[tile] = board[tile][0]
-    print(board)
     return board
 
+def check_solved(board): #Check if every tile only has 1 possible number
+    return all([isinstance(tile, int) for tile in board])
+
 board = example_board
+solved = False
+i = 0
 while not solved:
+    board = axes_prune(board)
+    board = format_board(board)
     board = subsquares_prune(board)
     board = format_board(board)
     solved = check_solved(board)
-    break
+    i += 1
+    if i > 999:
+        solved = True
+
+print(board[0:9])
+print(board[9:18])
+print(board[18:27])
+print(board[27:36])
+print(board[36:45])
+print(board[45:54])
+print(board[54:63])
+print(board[63:72])
+print(board[72:81])
